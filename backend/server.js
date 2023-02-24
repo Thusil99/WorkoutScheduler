e7c8826bb0dad82f85f2f346a0463e6dad4ea832
Thusil99 <tvishwaka@gmail.com>
2023-02-24 16:05:33 +0530

    application done

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
+
+//express app
+const app = express();
+
+//middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//routes
+app.use("/api/workouts", workoutRoutes);
+
+//db connection
+mongoose
+  .connect(process.env.MONGO_URI)
+
+  .then(() => {
+    //request listner
+    app.listen(process.env.PORT, () => {
+      console.log("rfasfs");
+    });
+  })
+
+  .catch((error) => {
+    console.log(error);
+  });
